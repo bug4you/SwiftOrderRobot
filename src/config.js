b11a2d79b29972b125ea59d4bf17b8f22e56c4c7
Dilshod Fayzullayev <brogrammer.devs@gmail.com>
2026-01-14 23:31:45 +0500

    Add environment configuration and update scripts for the Telegram bot

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ require('@dotenvx/dotenvx').config();
 
 const config = {
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
+    TELEGRAM_BOT_ADMIN_ID: process.env.TELEGRAM_BOT_ADMIN_ID || '',
 };
 
 export default config;
